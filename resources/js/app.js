95830022917ae2db96fe5c74043447bc7b8ec1c8
Yuki Sato <yuki.sato@sansan.com>
2021-02-22 15:53:53 +0900

    jqueryを使用したファイルの追加と読み込み

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import ExampleMobule from './module/ExampleMobule';
+import HelloTitle from './module/HelloTitle';
 
 let exampleMobule = new ExampleMobule();
+new HelloTitle();
 
 window.addEventListener('load', ()=>{
     exampleMobule.alert('Taro');
---
+++ resources/js/module/HelloTitle.js
@@ -0,0 +1,11 @@
+export default class HelloTitle {
+    constructor() {
+        this.$title = $('#js-title');
+    }
+
+    bindEvents() {
+        $(window).on('load', ()=>{
+            this.$title.text('Hello Taro!');
+        });
+    }
+}
