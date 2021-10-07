0f8635822a19bc767a980f376bad190de0d86960
Yuki Sato <yuki.sato@sansan.com>
2021-10-07 16:24:47 +0900

    linterの設定とPrettierの追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parser: "babel-eslint",
-  extends: [
-    "eslint:recommended"
-  ],
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: "module",
-  },
-  plugins: [],
-  globals: {},
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-}
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parser: 'babel-eslint',
+	extends: ['eslint:recommended', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 2019,
+		sourceType: 'module',
+	},
+	plugins: ['prettier'],
+	globals: {},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	},
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,65 +14,73 @@ fs.removeSync(`public/`);
 /**
  * 乱数生成
  */
-let getRandomStr = ()=>{
-  let base="abcdefghijklmnopqrstuvwxyz0123456789"// 使用する文字列
-  let num=16// 乱数の長さ
-  return Array.from(Array(num)).map(()=>base[Math.floor(Math.random()*base.length)]).join('');
+let getRandomStr = () => {
+	let base = 'abcdefghijklmnopqrstuvwxyz0123456789'; // 使用する文字列
+	let num = 16; // 乱数の長さ
+	return Array.from(Array(num))
+		.map(() => base[Math.floor(Math.random() * base.length)])
+		.join('');
 };
-const clearStr = getRandomStr();// キャッシュクリア パラメーター用
+const clearStr = getRandomStr(); // キャッシュクリア パラメーター用
 
 /**
  * ファイル管理用
- * 
- * @param {*} filePath 
+ *
+ * @param {*} filePath
  */
 let ejsMix = (filePath) => {
-  if (process.env.NODE_ENV === 'production') {
-    return filePath + '?id=' + clearStr;
-  }
-  return filePath;
-}
+	if (process.env.NODE_ENV === 'production') {
+		return filePath + '?id=' + clearStr;
+	}
+	return filePath;
+};
 
 mix
-.ejs('resources/views/**/*.ejs', 'public',
-{
-  mix: ejsMix,
-},{
-  root: 'resources/views',
-  base: 'resources/views',
-  partials: 'resources/views/partials'
-})
-// [polyfill](https://laravel-mix.com/extensions/polyfill)
-.polyfill({})
+	.ejs(
+		'resources/views/**/*.ejs',
+		'public',
+		{
+			mix: ejsMix,
+		},
+		{
+			root: 'resources/views',
+			base: 'resources/views',
+			partials: 'resources/views/partials',
+		},
+	)
+	// [polyfill](https://laravel-mix.com/extensions/polyfill)
+	.polyfill({})
 
-// javascript
-.js('resources/js/app.js', 'public/assets/js/')
-.eslint()
+	// javascript
+	.js('resources/js/app.js', 'public/assets/js/')
+	.eslint()
 
-// Scss
-.sass('resources/scss/style.scss', 'public/assets/css/')
-.options({
-    processCssUrls: false,
-  }
-)
-.webpackConfig({
-  plugins: [
-      new styleLintPlugin({
-          files: ['**/*.scss'],
-          configFile: path.join(__dirname, '.stylelintrc.js'),
-          syntax: 'scss'
-      })
-  ]
-})
+	// Scss
+	.sass('resources/scss/style.scss', 'public/assets/css/')
+	.options({
+		processCssUrls: false,
+		postCss: [
+			require('autoprefixer')({
+				grid: 'autoplace',
+			}),
+		],
+	})
+	.webpackConfig({
+		plugins: [
+			new styleLintPlugin({
+				files: ['resources/**/*.scss'],
+				configFile: path.join(__dirname, '.stylelintrc.js'),
+				syntax: 'scss',
+			}),
+		],
+	})
 
-.sourceMaps(false, 'inline-cheap-module-source-map')// cssのマップを出力ファイルに追記する形で用意する
-.copy(`resources/images/`, `public/assets/images`)
-// browserSync
-// URL: https://browsersync.io/docs/options/
-.browserSync(
-  {
-    files: 'public/**/*',
-    server: 'public',
-    proxy: false
-  }
-);
+	.sourceMaps(false, 'inline-cheap-module-source-map') // cssのマップを出力ファイルに追記する形で用意する
+	.copy(`resources/images/`, `public/assets/images`)
+	// browserSync
+	// URL: https://browsersync.io/docs/options/
+	.browserSync({
+		files: 'public/**/*',
+		server: 'public',
+		proxy: false,
+	});
